95445a60349decd5e79c5f68a025f8a4c1df1fbe
prakasg reddy kadira <kp.reddy.999@gmail.com>
2026-07-30 15:44:35 -0400

    Fixed Json string to Enum Converter and removed default classes

--- src/AccountService/AccountService.Api/Program.cs
+++ src/AccountService/AccountService.Api/Program.cs
@@ -6,8 +6,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
-
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 // Infrastructure: DbContext + repositories
 builder.Services.AddAccountServiceInfrastructure(builder.Configuration);
 
